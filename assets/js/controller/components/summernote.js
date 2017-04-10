5b9c8c36ffffb99fecf87d8a942d7a3e2481ffa2
jackyu <jackyu@DESKTOP-KOT7AGF>
2017-04-10 10:01:48 +0800

    富文本框JS创建

---
+++ assets/js/controller/components/summernote.js
@@ -0,0 +1,20 @@
+/**
+ * 作者：yujinjin9@126.com
+ * 时间：2017-04-10
+ * 描述：富文本框
+ */
+define(function () {
+	var controller = {
+        init: function () {
+        	this.bindEvent();
+        },
+        bindEvent: function() {
+        	var _this = this;
+        }
+    }
+	return {
+        init: function () {
+            controller.init();
+        }
+    }
+});
